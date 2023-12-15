e13245888c81cb3f6c7b8706dedfd828a02f5516
Emily Krohn <emilykrohn2@gmail.com>
2023-12-15 15:01:36 -0800

    Add dash to player movement script

--- Scripts/Player/PlayerMovement.gd
+++ Scripts/Player/PlayerMovement.gd
@@ -1,11 +1,32 @@
 extends Node
 
-func movement(speed):
+const dash_speed = 500
+var cooldown = true
+var timer = Timer.new()
+
+func _ready():
+	timer.timeout.connect(_on_timer_timeout)
+	timer.wait_time = 0.15
+	add_child(timer)
+
+func movement():
 	var horizontal_direction = Input.get_axis("ui_left", "ui_right")
 	var vertical_direction = Input.get_axis("ui_up", "ui_down")
-
 	
+	if Input.is_action_just_pressed("ui_select") and cooldown:
+		get_parent().speed += dash_speed
+		cooldown = false
+		start_cooldown()
+
 	if horizontal_direction or vertical_direction:
-		get_parent().velocity = Vector2(horizontal_direction, vertical_direction) * speed
+		get_parent().velocity = Vector2(horizontal_direction, vertical_direction).normalized() * get_parent().speed
 	else:
 		get_parent().velocity = Vector2(0,0)
+
+func start_cooldown():
+	timer.start()
+
+func _on_timer_timeout():
+	if cooldown == false:
+		get_parent().speed -= dash_speed
+		cooldown = true
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
+var speed = 200
+
 func _physics_process(delta):
-	%PlayerMovement.movement(200)
+	%PlayerMovement.movement()
 
 	move_and_slide()
