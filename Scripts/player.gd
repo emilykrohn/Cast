c473b7e9be692a06292df9c520d8f5ecdad359f9
Emily Krohn <emilykrohn2@gmail.com>
2023-12-15 04:15:40 -0800

    Player can move up and down

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,10 +5,11 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta):
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var horizontal_direction = Input.get_axis("ui_left", "ui_right")
+	var vertical_direction = Input.get_axis("ui_up", "ui_down")
+	if horizontal_direction or vertical_direction:
+		velocity = Vector2(horizontal_direction, vertical_direction) * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = Vector2(0,0)
 
 	move_and_slide()
